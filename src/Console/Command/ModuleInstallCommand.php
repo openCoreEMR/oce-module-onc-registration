b5293479188f0cbf2911ef28d9b05577dd249f02
Michael A. Smith <michael@opencoreemr.com>
2026-05-08 09:55:35 -0400

    fix: add declare(strict_types=1) to satisfy Rector
    Rector's SafeDeclareStrictTypesRector flagged 22 files missing
    declare(strict_types=1);. Inserted between the file docblock and
    namespace per PSR12 file header ordering.

--- src/Console/Command/ModuleInstallCommand.php
+++ src/Console/Command/ModuleInstallCommand.php
@@ -10,6 +10,8 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+declare(strict_types=1);
+
 // TEMPLATE: Update namespace to match your module
 namespace OpenCoreEMR\Modules\OncRegistration\Console\Command;
 
